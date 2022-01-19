8e718e915f52537b1c082fb90da85a4827535e54
David <Davidmaxsands@gmail.com>
2022-01-18 19:05:55 -0700

    commit tree dffa7bbc9693955c6d61c980822c0d9c776e3deb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const input = e.target.children[1].value;
+      fetch(`http://localhost:3000/movies/${input}`)
+      .then(res => res.json())
+      .then(movies => {
+          const title = document.querySelector('section#movieDetails h4');
+          const summary = document.querySelector('section#movieDetails p')
+          title.innerText = movies.title;
+          summary.innerText = movies.summary;
+      })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
